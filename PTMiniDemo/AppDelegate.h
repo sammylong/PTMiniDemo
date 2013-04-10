e8b770af25b671777a0da3b7ef8e26f4bd23c692
Sammy Long <dragonbabe22@gmail.com>
2013-04-10 12:33:41 +0800

    Initial Commit

---
+++ PTMiniDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PTMiniDemo
+//
+//  Created by Sammy Long on 13/4/10.
+//  Copyright (c) 2013年 Apexlearn Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ PTMiniDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PTMiniDemo
+//
+//  Created by Sammy Long on 13/4/10.
+//  Copyright (c) 2013年 Apexlearn Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
